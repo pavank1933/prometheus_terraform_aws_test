92214b4d2e18cde941a68541bc4580ed2f83c756
root <root@ip-172-31-36-135.us-east-2.compute.internal>
2018-07-02 17:21:25 +0000

    unzip added

--- install_terraform.sh
+++ install_terraform.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 curl -O https://releases.hashicorp.com/terraform/0.8.2/terraform_0.8.2_linux_amd64.zip
+apt-get install unzip -y
 unzip terraform*
 mv terraform /usr/local/bin/
 rm -rf terraform*
+export AWS_DEFAULT_REGION="us-east-2"
